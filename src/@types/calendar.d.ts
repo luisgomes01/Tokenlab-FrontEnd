3f07c448af792cc4e1c01b191d62bb95512d5394
Luís <luis252001@hotmail.com>
2021-09-07 20:42:17 -0300

    Creating Calendar Context

--- src/@types/calendar.d.ts
+++ src/@types/calendar.d.ts
@@ -8,3 +8,11 @@ interface ICalendar {
   name: string;
   color: string;
 }
+
+interface CalendarContext {
+  events: EventAgenda[];
+  setEvents: (events: EventAgenda[]) => void;
+  addNewEvent: (event: EventAgenda) => void;
+  removeEvent: (eventId: number) => void;
+  editEvent: (event: EventAgenda) => void;
+}
